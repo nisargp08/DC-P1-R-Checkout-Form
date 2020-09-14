31c4a6888298e9c52a2de6b6e78754e3d3447204
Nisarg Patel <nisargp42@gmail.com>
2020-09-14 01:25:20 -0400

    Design Done

--- tailwind.js
+++ tailwind.js
@@ -1,7 +1,38 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+        'montserrat' : ['Montserrat','sans-serif'],
+      },
+      borderRadius : {
+        '12px' : '12px',
+        '13px' : '13px',
+      },
+      backgroundColor : {
+        'gray-5' : '#E0E0E0',
+        'gray-6' : '#F2F2F2',
+      },
+      textColor : {
+        'black-1' : '#4E5150',
+        'gray-1' : '#333333',
+        'gray-2' : '#4F4F4F',
+        'gray-3' : '#828282',
+        'footer' : '#A9A9A9',
+      },
+      borderColor : {
+        'gray-3' : '#828282',
+        'gray-4' : '#BDBDBD',
+        'black-2' : '#828282',
+        'footer' : '#A9A9A9',
+      },
+      fontSize : {
+        '10px' : '10px',
+      },
+      width : {
+        '48' : '48%',
+      }
+    },
   },
   variants: {},
   plugins: [],
